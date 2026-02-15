c20145f8cc36054740d4cc9fce619f5767c64680
Ronen Druker <4226710+rdrkr@users.noreply.github.com>
2026-02-15 11:00:15 +0100

    Automated backup: Update dotfiles

---
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -0,0 +1,7 @@
+-- Plugin: mfussenegger/nvim-dap
+-- Installed via store.nvim
+
+return {
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy"
+}
--- .config/nvim/lua/plugins/py-requirements.nvim.lua
+++ .config/nvim/lua/plugins/py-requirements.nvim.lua
@@ -4,11 +4,46 @@
 return {
     "MeanderingProgrammer/py-requirements.nvim",
     dependencies = {
-        "nvim-treesitter/nvim-treesitter"
+        "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-        require(
-            "py-requirements"
-        ).setup({})
-    end
+        require("py-requirements").setup({
+            -- Endpoint used for getting package versions
+            index_url = "https://pypi.org/simple/",
+            -- Fallback endpoint in case 'index_url' fails to find a package
+            extra_index_url = nil,
+            -- Specify which file patterns plugin is active on
+            -- For info on patterns, see :h pattern
+            file_patterns = { ".*requirements.*.txt", ".*pyproject.*.toml" },
+            -- Options for how diagnostics are displayed
+            diagnostic_opts = { padding = 5 },
+            -- For available options, see :h vim.lsp.util.open_floating_preview
+            float_opts = { border = "rounded" },
+            filter = {
+                -- Pull only final release versions, this will ignore alpha, beta,
+                -- release candidate, post release, and developmental release versions
+                final_release = false,
+                -- Ignore yanked package versions
+                yanked = true,
+            },
+            -- Enabled by default if you want to disable lsp completions set to false
+            enable_lsp = true,
+            keys = {
+                {
+                    "<leader>ru",
+                    function()
+                        require("py-requirements").upgrade()
+                    end,
+                    desc = "Upgrade dep under cursor",
+                },
+                {
+                    "<leader>rU",
+                    function()
+                        require("py-requirements").upgrade_all()
+                    end,
+                    desc = "Upgrade all deps",
+                },
+            },
+        })
+    end,
 }
