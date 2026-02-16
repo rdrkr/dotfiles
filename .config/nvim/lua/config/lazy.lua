7ae94cf08af238d39904571f940efa84f646aaac
Ronen Druker <4226710+rdrkr@users.noreply.github.com>
2026-02-16 11:00:13 +0100

    Automated backup: Update dotfiles

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -22,6 +22,8 @@ require("lazy").setup({
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         { import = "lazyvim.plugins.extras.ai.copilot" },
         { import = "lazyvim.plugins.extras.dap.core" },
+        { import = "lazyvim.plugins.extras.lang.typescript" },
+        { import = "lazyvim.plugins.extras.lang.json" },
         { import = "plugins" },
     },
     defaults = {
