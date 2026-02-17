2a84dedc6368ecfbdfffff3e404670bf50fb07aa
Ronen Druker <4226710+rdrkr@users.noreply.github.com>
2026-02-18 00:34:15 +0100

    Automated backup: Update dotfiles

---
+++ .config/nvim/lua/plugins/github-actions.nvim.lua
@@ -0,0 +1,11 @@
+-- Plugin: skanehira/github-actions.nvim
+-- Installed via store.nvim
+
+return {
+    "skanehira/github-actions.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim" -- Optional: for enhanced workflow selection
+    },
+    opts = {}
+}
