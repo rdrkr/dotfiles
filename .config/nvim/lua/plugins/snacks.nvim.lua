a3369b1ac418b646555f756e2f14fed03c9839fa
Ronen Druker <4226710+rdrkr@users.noreply.github.com>
2026-03-06 10:00:16 +0100

    Automated backup: Update dotfiles

---
+++ .config/nvim/lua/plugins/snacks.nvim.lua
@@ -0,0 +1,17 @@
+-- Plugin: folke/snacks.nvim
+-- Installed via store.nvim
+
+return {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    keys = {
+        {
+            "<leader>lg",
+            function()
+                Snacks.lazygit()
+            end,
+            desc = "LazyGit",
+        },
+    },
+}
