8166fe958eb5d8ca7f4eaa8465bdbeb8da350d37
Ronen Druker <4226710+rdrkr@users.noreply.github.com>
2026-03-23 13:00:13 +0100

    Automated backup: Update dotfiles

--- .config/nvim/lua/plugins/apple-notes.nvim.lua
+++ .config/nvim/lua/plugins/apple-notes.nvim.lua
@@ -4,18 +4,12 @@ return {
         "nvim-telescope/telescope.nvim", -- optional: for note picker
         "nvim-neo-tree/neo-tree.nvim", -- optional: for tree sidebar
     },
-    cmd = {
-        "AppleNotes",
-        "AppleNotesSearch",
-        "AppleNotesNew",
-        "AppleNotesQuick",
-        "AppleNotesTree",
-    },
     keys = {
         { "<leader>anf", "<cmd>AppleNotes<CR>", desc = "Find Apple Note" },
         { "<leader>ans", "<cmd>AppleNotesSearch<CR>", desc = "Search Apple Notes" },
         { "<leader>ann", "<cmd>AppleNotesNew<CR>", desc = "New Apple Note" },
         { "<leader>ant", "<cmd>AppleNotesTree<CR>", desc = "Toggle Apple Notes tree" },
     },
+    event = "VeryLazy",
     opts = {},
 }
