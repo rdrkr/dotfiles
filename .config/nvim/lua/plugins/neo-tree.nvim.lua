7bd1dd3f8435db8f21abe949eb46ec826ebf94bf
Ronen Druker <4226710+rdrkr@users.noreply.github.com>
2026-03-23 14:00:12 +0100

    Automated backup: Update dotfiles

--- .config/nvim/lua/plugins/neo-tree.nvim.lua
+++ .config/nvim/lua/plugins/neo-tree.nvim.lua
@@ -7,15 +7,34 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
-        "nvim-tree/nvim-web-devicons" -- optional, but recommended
+        "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
     lazy = false, -- neo-tree will lazily load itself
     opts = {
+        sources = {
+            "filesystem",
+            "buffers",
+            "git_status",
+            "apple-notes",
+        },
         filesystem = {
             filtered_items = {
                 visible = true,
                 hide_dotfiles = false,
                 hide_gitignored = false,
+            },
+        },
+        ["apple-notes"] = {
+            window = {
+                mappings = {
+                    ["o"] = "open",
+                    ["a"] = "add",
+                    ["A"] = "add_directory",
+                    ["d"] = "delete",
+                    ["r"] = "rename",
+                    ["m"] = "move",
+                    ["R"] = "refresh",
+                },
             },
         },
     },
